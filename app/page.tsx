a7f4ebf1aab4fd0846a17c9bd621c7f6e8a6c0e3
KhantWaiLin <khantwailin@bimgoc.com>
2024-02-06 09:17:45 +0630

    cursor live updating done

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+"use client";
+
+import Live from "@/components/Live";
+
 export default function Page() {
   return (
     <div className="w-full h-[100vh] flex justify-center items-center text-center">
-      <h1 className="text-2xl text-white">Live Block Figma Clone</h1>;
+      <Live />
     </div>
   );
 }
