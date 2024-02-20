7dde253d7a81e1486b35b376ffa4e8b263a4a369
KhantWaiLin <khantwailin@bimgoc.com>
2024-02-20 09:25:04 +0630

    adding live reaction using liveblock useEventLiser and broadcast

--- components/Live.tsx
+++ components/Live.tsx
@@ -1,8 +1,13 @@
-import { useMyPresence, useOthers } from "@/liveblocks.config";
+import {
+  useBroadcastEvent,
+  useEventListener,
+  useMyPresence,
+  useOthers,
+} from "@/liveblocks.config";
 import LiveCursors from "./cursor/LiveCursors";
 import { useCallback, useEffect, useState } from "react";
 import CursorChat from "./cursor/CursorChat";
-import { CursorMode, CursorState, Reaction } from "@/types/type";
+import { CursorMode, CursorState, Reaction, ReactionEvent } from "@/types/type";
 import ReactionSelector from "./reaction/ReactionButton";
 import FlyingReaction from "./reaction/FlyingReaction";
 import useInterval from "@/hooks/useInterval";
@@ -57,6 +62,14 @@ const Live = () => {
     [cursorState.mode, setCursorState]
   );
 
+  const broadcast = useBroadcastEvent();
+
+  useInterval(() => {
+    setReactions((reaction) =>
+      reaction.filter((r) => r.timestamp > Date.now() - 4000)
+    );
+  }, 1000);
+
   useInterval(() => {
     if (
       cursorState.mode === CursorMode.Reaction &&
@@ -72,8 +85,27 @@ const Live = () => {
           },
         ])
       );
+
+      broadcast({
+        x: cursor.x,
+        y: cursor.y,
+        value: cursorState.reaction,
+      });
     }
   }, 100);
+
+  useEventListener((eventData) => {
+    const event = eventData.event as ReactionEvent;
+    setReactions((reactions) =>
+      reactions.concat([
+        {
+          point: { x: event.x, y: event.y },
+          value: event.value,
+          timestamp: Date.now(),
+        },
+      ])
+    );
+  });
 
   useEffect(() => {
     const onKeyUp = (e: KeyboardEvent) => {
