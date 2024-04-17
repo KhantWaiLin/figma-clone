4eb63ceaf678623d670c5e428c9fe638fd42286b
KhantWaiLin <khantkhant.cm@gmail.com>
2024-04-17 12:35:50 +0700

    fix type error in roomprovider

--- app/Room.tsx
+++ app/Room.tsx
@@ -12,8 +12,7 @@ export function Room({ children }: { children: ReactNode }) {
       id="my-room"
       initialPresence={{
         cursor: null,
-        cursorColor: null,
-        editingText: null,
+        message: ""
       }}
       initialStorage={{
         canvasObjects: new LiveMap()
